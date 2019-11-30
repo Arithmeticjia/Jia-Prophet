0260d427fa21e6e365dc54f855510398856d1eed
ArithmeticJia <1097197237@qq.com>
2019-11-30 23:17:09 +0800

    flask-vue

--- jia-vue/config/index.js
+++ jia-vue/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../../static'),
     assetsSubDirectory: 'vue',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/static',
 
     /**
      * Source Maps
