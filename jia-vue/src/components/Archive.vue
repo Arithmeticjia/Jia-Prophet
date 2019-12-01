cf0c2e7759def19c7cc971f3abc3bab89f22e7ab
ArithmeticJia <1097197237@qq.com>
2019-12-01 21:36:32 +0800

    flask-vue

--- jia-vue/src/components/Archive.vue
+++ jia-vue/src/components/Archive.vue
@@ -1,12 +1,74 @@
 <template>
-  <div>
-    <div class="column" v-for="blog in bloglist">
-          <h2>
-            {{ blog.title }}
-          </h2>
-            <p>{{ blog.content }}.</p>
-    </div>
-<!--      <button @click="getData">GET DATA</button>-->
+<!--    <nav class="navbar navbar-fixed-top navbar-inverse">-->
+<!--      <div class="container">-->
+<!--        <div class="navbar-header">-->
+<!--          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">-->
+<!--            <span class="sr-only">Toggle navigation</span>-->
+<!--            <span class="icon-bar"></span>-->
+<!--            <span class="icon-bar"></span>-->
+<!--            <span class="icon-bar"></span>-->
+<!--          </button>-->
+<!--          <a class="navbar-brand" href="#">Project name</a>-->
+<!--        </div>-->
+<!--        <div id="navbar" class="collapse navbar-collapse">-->
+<!--          <ul class="nav navbar-nav">-->
+<!--            <li class="active"><a href="#">Home</a></li>-->
+<!--            <li><a href="#about">About</a></li>-->
+<!--            <li><a href="#contact">Contact</a></li>-->
+<!--          </ul>-->
+<!--        </div>&lt;!&ndash; /.nav-collapse &ndash;&gt;-->
+<!--      </div>&lt;!&ndash; /.container &ndash;&gt;-->
+<!--    </nav>&lt;!&ndash; /.navbar &ndash;&gt;-->
+
+  <div class="container">
+
+      <div class="row row-offcanvas row-offcanvas-right">
+
+        <div class="col-xs-12 col-sm-9">
+          <p class="pull-right visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
+          </p>
+          <div class="jumbotron">
+            <h1>Hello, world!</h1>
+            <p>This is an example to show the potential of an offcanvas layout pattern in Bootstrap. Try some responsive-range viewport sizes to see it in action.</p>
+          </div>
+          <div class="row">
+            <div class="col-xs-6 col-lg-4" v-for="blog in bloglist">
+              <h2>{{ blog.title }}</h2>
+              <p>{{ blog.content }}</p>
+              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+            </div><!--/.col-xs-6.col-lg-4-->
+          </div><!--/row-->
+        </div><!--/.col-xs-12.col-sm-9-->
+
+        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar">
+          <div class="list-group">
+            <a href="#" class="list-group-item active">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+            <a href="#" class="list-group-item">Link</a>
+          </div>
+        </div><!--/.sidebar-offcanvas-->
+      </div><!--/row-->
+<!--  <div class="container">-->
+<!--    <div class="column" v-for="blog in bloglist">-->
+<!--          <h2>-->
+<!--            {{ blog.title }}-->
+<!--          </h2>-->
+<!--            <p>{{ blog.content }}.</p>-->
+<!--    </div>-->
+<!--&lt;!&ndash;      <button @click="getData">GET DATA</button>&ndash;&gt;-->
+    <hr>
+
+      <footer>
+        <p>&copy; 2016 Company, Inc.</p>
+      </footer>
   </div>
 </template>
 
@@ -17,13 +79,16 @@
         name: "Archive",
         data: function() {
             return {
-              bloglist: []
+              bloglist: [],
+              // list: []
             }
         },
         created(){
             var that = this;
             axios.get('http://127.0.0.1:5000/api/bloglist').then(function (response) {
                 that.bloglist = response.data;
+            }).catch(function (error) {
+                alert('Error ' + error);
             })
         },
         methods: {
@@ -48,9 +113,7 @@
 </script>
 
 <style scoped>
-  /* 创建三个相等的列 */
-.column {
-  float: left;
-  width: 33.33%;
-}
+  @import '../assets/dist/css/bootstrap.min.css';
+  @import '../assets/dist/css/bootstrap.css';
+  @import '../assets/dist/css/offcanvas.css';
 </style>
