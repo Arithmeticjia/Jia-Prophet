eca7d13343737b9bfbc146c090a9eaf876fcd9b1
ArithmeticJia <1097197237@qq.com>
2019-12-01 18:15:32 +0800

    flask-vue

--- jia-vue/src/components/Archive.vue
+++ jia-vue/src/components/Archive.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-      <span>{{ serverResponse }} </span>
-      <button @click="getData">GET DATA</button>
+    <div class="columns medium-3" v-for="blog in list">
+        <div class="card">
+          <div class="card-divider">
+            {{ blog.title }}
+          </div>
+          <div class="card-section">
+            <p>{{ blog.content }}.</p>
+          </div>
+        </div>
     </div>
+      <button @click="getData">GET DATA</button>
+  </div>
 </template>
 
 <script>
@@ -12,23 +21,22 @@
         name: "Archive",
         data: function() {
             return {
-              serverResponse: 'resp'
+              list: []
             }
         },
         methods: {
           getData() {
             var that = this;
             // 对应 Python 提供的接口，这里的地址填写下面服务器运行的地址，本地则为127.0.0.1，外网则为 your_ip_address
-            const path = 'http://127.0.0.1:5000/api/blogdetail';
+            const path = 'http://127.0.0.1:5000/api/bloglist';
             axios.get(path).then(function (response) {
               // 这里服务器返回的 response 为一个 json object，可通过如下方法需要转成 json 字符串
               // 可以直接通过 response.data 取key-value
               // 坑一：这里不能直接使用 this 指针，不然找不到对象
-              var content = response.data.content;
               // 坑二：这里直接按类型解析，若再通过 JSON.stringify(msg) 转，会得到带双引号的字串
-              that.serverResponse = content;
-
-              alert('Success ' + response.status + ', ' + response.data + ', ' + content);
+              that.list = response.data;
+              // alert('Success ' + response.status + ', ' + response.data + ', ' + content);
+                console.log(that.list)
             }).catch(function (error) {
               alert('Error ' + error);
             })
